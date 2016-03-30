991d444bc5f73445de84ef6ff0735b184d7fcebf
TApplencourt <tapplencourt@gmail.com>
2016-03-30 19:23:02 +0200

    yapf

--- src/calc.py
+++ src/calc.py
@@ -6,6 +6,7 @@ from src.SQL_util import cond_sql_or
 from collections import namedtuple
 from src.__init__ import lru_cache, zipdic
 
+
 class BigData(object):
 
     # ___           
@@ -13,7 +14,7 @@ class BigData(object):
     # _|_ | | |  |_ 
     #
 
-    def __init__(self,d_arguments,run_id_ref=None):
+    def __init__(self, d_arguments, run_id_ref=None):
         self.d_arguments = d_arguments
         self.run_id_ref = int(run_id_ref)
 
@@ -27,20 +28,20 @@ class BigData(object):
              "basis": "--basis",
              "method": "--method",
              "comments": "--comments"}
-    
+
         # If the user give ask for somme run_id return it
         l_cond_filter = []
-        
+
         for k, v in d.items():
             try:
                 l_cond_filter += cond_sql_or(k, self.d_arguments[v])
             except KeyError:
                 pass
-    
+
         l_cond_filter = l_cond_filter if l_cond_filter else ["(1)"]
 
         sql_cmd_where = " AND ".join(l_cond_filter)
-    
+
         # -#-#- #
         # S Q L #
         # -#-#- #
@@ -53,24 +54,23 @@ class BigData(object):
                                    WHERE {0}
                                """.format(sql_cmd_where))
 
-
         RunInfo = namedtuple('RunInfo', 'run_id, method, basis, geo, comments')
 
-        d_run_info=dict()
+        d_run_info = dict()
         for emp in map(RunInfo._make, cursor.fetchall()):
             d_run_info[emp.run_id] = emp
 
         if not d_run_info:
             print "No run_id with:", sql_cmd_where
             sys.exit(1)
-    
+
         return d_run_info
 
     @property
     @lru_cache(maxsize=1)
     def l_run_id(self):
         return self.d_run_info.keys()
-    
+
     @property
     @lru_cache(maxsize=1)
     def d_formula(self):
@@ -85,12 +85,12 @@ class BigData(object):
         All the element need to have a formula!
         """
         d = dict()
-    
+
         c.execute("""SELECT name, formula
                                    FROM ele_tab""")
-    
-        d = {name: [a*nb for a,nb in eval(f)] for (name, f) in c.fetchall()}
-    
+
+        d = {name: [a * nb for a, nb in eval(f)] for (name, f) in c.fetchall()}
+
         return d
 
     @property
@@ -98,7 +98,6 @@ class BigData(object):
     def l_element_whe_want(self):
 
         from src.db_interface import db_list_element
-
         """
         Input
         d_arguments: docot dict of arguments
@@ -107,22 +106,22 @@ class BigData(object):
         l_ele:  list of element who satisfy the condition
         If we need to get all the element, l_ele = "*"
         """
-    
+
         if self.d_arguments["--ele"]:
             l_ele = self.d_arguments["--ele"]
         elif self.d_arguments["--like-sr7"]:
-            l_ele = ["MnCl","ZnCl","FeCl","CrCl","ZnS","ZnH","CuCl"]
+            l_ele = ["MnCl", "ZnCl", "FeCl", "CrCl", "ZnS", "ZnH", "CuCl"]
         elif self.d_arguments["--like-mr13"]:
-            l_ele = ["ZnO","NiCl","TiCl","CuH","VO","VCl","MnS","CrO","CoH","CoCl","VH","FeH","CrH"]
+            l_ele = ["ZnO", "NiCl", "TiCl", "CuH", "VO", "VCl", "MnS", "CrO",
+                     "CoH", "CoCl", "VH", "FeH", "CrH"]
         elif self.d_arguments["--like_run_id"]:
             l_ele = db_list_element(self.d_arguments["--like_run_id"])
         else:
             l_ele = ["*"]
 
         if self.d_arguments["--all_children"] and not l_ele == ["*"]:
-            l_ele = list(set(l_ele + [a for ele in l_ele for a in self.d_formula[ele]]))
-        
-        
+            l_ele = list(set(
+                l_ele + [a for ele in l_ele for a in self.d_formula[ele]]))
 
         return l_ele
 
@@ -132,22 +131,23 @@ class BigData(object):
 
         from collections import defaultdict
 
-        l = cond_sql_or("ele_name",self.l_element_whe_want)
+        l = cond_sql_or("ele_name", self.l_element_whe_want)
         l += cond_sql_or("run_id", self.l_run_id)
         sql_cmd_where = " AND ".join(l)
-        
-        cursor =  c_row.execute("""SELECT run_id,
+
+        cursor = c_row.execute("""SELECT run_id,
                                           ele_name
                                    FROM run_tab_ele
                                    WHERE {0}
                                 """.format(sql_cmd_where))
-    
+
         d_run_id_ele = defaultdict(list)
-        for run_id,ele in cursor:
+        for run_id, ele in cursor:
             d_run_id_ele[run_id].append(ele)
-    
-        assert d_run_id_ele, "No run have any element you request: {0}".format(self.l_element_whe_want)
-    
+
+        assert d_run_id_ele, "No run have any element you request: {0}".format(
+            self.l_element_whe_want)
+
         return d_run_id_ele
 
     @property
@@ -155,31 +155,30 @@ class BigData(object):
     def l_element(self):
         return list(set.union(*map(set, self.d_l_element.values())))
 
-    
-    def db_get(self,table_name):
+    def db_get(self, table_name):
 
-        l = cond_sql_or("run_id",self.l_run_id)
-        l += cond_sql_or("ele_name",self.l_element)
+        l = cond_sql_or("run_id", self.l_run_id)
+        l += cond_sql_or("ele_name", self.l_element)
         sql_cmd_where = " AND ".join(l)
-    
+
         cursor = c_row.execute("""SELECT *
                                   FROM {0}
-                                 WHERE {1}""".format(table_name,sql_cmd_where))
+                                 WHERE {1}"""
+                               .format(table_name, sql_cmd_where))
 
         from collections import defaultdict
         from src.object import Energie
 
         d = defaultdict(dict)
-    
+
         for r in cursor:
-            
+
             e = r["energy"]
             err = r["err"] if r["err"] else 0
-    
-            d[r["run_id"]][r["ele_name"]] = Energie(e,err)
-    
-        return d
 
+            d[r["run_id"]][r["ele_name"]] = Energie(e, err)
+
+        return d
 
     @property
     @lru_cache(maxsize=1)
@@ -205,15 +204,16 @@ class BigData(object):
         from collections import defaultdict
 
         d = defaultdict(dict)
-    
-        for run_id,d_mol in self.d_e.items():
+
+        for run_id, d_mol in self.d_e.items():
             for ele, energy in d_mol.items():
-    
+
                 if len(self.d_formula[ele]) == 1:
                     continue
 
                 try:
-                    d[run_id][ele] = sum(d_mol[i] for i in self.d_formula[ele]) - d_mol[ele]
+                    d[run_id][ele] = sum(
+                        d_mol[i] for i in self.d_formula[ele]) - d_mol[ele]
                 except KeyError:
                     pass
 
@@ -224,7 +224,7 @@ class BigData(object):
     def d_ae(self):
         d = self.d_ae_db.copy()
         d.update(self.d_ae_calc)
- 
+
         return d
 
     @property
@@ -241,13 +241,14 @@ class BigData(object):
 
         d = defaultdict(dict)
 
-
         assert self.run_id_ref, "You need to set a run reference for compute the deviation"
-        assert self.run_id_ref in self.l_run_id, "{0} need to be in {1}".format(self.run_id_ref,self.l_run_id)
+        assert self.run_id_ref in self.l_run_id, "{0} need to be in {1}".format(
+            self.run_id_ref, self.l_run_id)
 
-        for run_id,d_ae_run_id in self.d_ae.items():
-    
-            for ele, energy, energy_ref in zipdic(d_ae_run_id,self.d_ae[self.run_id_ref]):
+        for run_id, d_ae_run_id in self.d_ae.items():
+
+            for ele, energy, energy_ref in zipdic(d_ae_run_id,
+                                                  self.d_ae[self.run_id_ref]):
                 d[run_id][ele] = energy - energy_ref
 
         return d
@@ -256,15 +257,18 @@ class BigData(object):
     @lru_cache(maxsize=1)
     def d_mad(self):
         d = {}
-        for run_id,d_ele in self.d_ae_deviation.items():
+        for run_id, d_ele in self.d_ae_deviation.items():
             l_energy = d_ele.values()
             mad = sum(map(abs, l_energy)) / len(l_energy)
             d[run_id] = mad
-        return d    
+        return d
+
 
 if __name__ == '__main__':
-    d_arguments={"--run_id":[1,8],"--ele":["MnCl"],"--all_children":True}
+    d_arguments = {"--run_id": [1, 8],
+                   "--ele": ["MnCl"],
+                   "--all_children": True}
 
-    q = BigData(d_arguments=d_arguments,run_id_ref="1")
+    q = BigData(d_arguments=d_arguments, run_id_ref="1")
     print q.d_ae
     print q.d_ae_deviation
